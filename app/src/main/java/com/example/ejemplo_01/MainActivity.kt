12251f52f733ce0a7e64e83c3653509f7e5b718d
vnhrr <ivan.herrero.galvan@iesjulianmarias.es>
2024-10-04 15:55:26 +0200

    Inicial

--- app/src/main/java/com/example/ejemplo_01/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo_01/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+// El metodo onCreate es el unico que es obligado su sobreescritura, heredado de AppCompactActiviti
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
